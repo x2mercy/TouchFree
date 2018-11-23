f401a35df15da77a447b6783d1b8623cd2c96423
Judge Lee <jblee@hmc.edu>
2018-11-23 10:36:25 -0500

    Adding My Recipes tab

--- EasyCook/app/src/main/java/com/example/chenrui/easycook/NavigateActivity.java
+++ EasyCook/app/src/main/java/com/example/chenrui/easycook/NavigateActivity.java
@@ -1,16 +1,22 @@
 package com.example.chenrui.easycook;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class NavigateActivity extends AppCompatActivity {
+public class NavigateActivity extends AppCompatActivity implements UserProfile.UserProfileListener,Tab2Recipes.OnFragmentInteractionListener{
 
     //TODO fragments initialization
+    public Recipes recipes;
+
+    private FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,12 @@ public class NavigateActivity extends AppCompatActivity {
         // -search -shopping list -my recipe
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.navigation);
+
+        // Added by Justin for My Recipes tab
+        recipes = new Recipes();
+        fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.frag_navigation,recipes);
 
         // Set Item click listener to the menu items
         bottomNavigationView.setOnNavigationItemSelectedListener(
@@ -49,5 +61,15 @@ public class NavigateActivity extends AppCompatActivity {
                         return false;
                     }
                 });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void pickRecipeList(int i) {
+        recipes.pickRecipeList(i);
     }
 }
